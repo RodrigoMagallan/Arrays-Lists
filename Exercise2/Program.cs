d7fc7d7764d5dd0a4c2c14974e6eca63b5404508
Roydrigo <lrmagallanj513@gmail.com>
2022-06-10 16:29:10 -0500

    String reverse

---
+++ Exercise2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Pon tu palabra que quieras voltear: ");
+string notReversed = Console.ReadLine();
+
+
+
+var reversed = notReversed.ToCharArray().Reverse();
+Console.WriteLine(new string(reversed.ToArray()));
+
+
