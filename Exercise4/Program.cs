f2925f5f3fdad072b0ad2bb0003a2607342bd4f8
Roydrigo <lrmagallanj513@gmail.com>
2022-06-14 14:09:23 -0500

    Exercise 4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,29 @@
+﻿//write a program and ask the user to enter a number or type "Quit" to exit. The list of number bay include duplicates. Display the unique numbers that the user has entered
+
+var newList = new List<int>();
+
+string quit = "quit";
+while (true)
+{
+    Console.WriteLine("Enter a number or type quit to display: ");
+
+    var thingEntered = Console.ReadLine();
+    if (!thingEntered.Equals(quit))
+    {
+        var numberEntered = Convert.ToInt32(thingEntered);
+        newList.Add(numberEntered);
+        continue;
+    }
+    else
+    {
+        break;
+    }
+}
+
+
+newList = newList.Distinct().ToList();
+Console.WriteLine("You list: ");
+foreach (var n in newList)
+{
+    Console.WriteLine(n);
+}
